47423e4fbce9dd591256e6856190048cb42aee87
Casen Luck <casenluck@gmail.com>
2021-12-09 11:13:11 -0500

    working views and add role and add department

--- javascript.js
+++ javascript.js
@@ -3,7 +3,8 @@ const mysql = require('mysql2/promise');
 const cTable = require('console.table');
 const questions = require('./src/questions');
 
-
+let table;
+let departList;
 
 async function start() {
 
@@ -18,65 +19,85 @@ async function start() {
         },
         console.log(`Connected to the company_db database.`)
       );
-      const departments = async () => {
-        const table = await db.query('SELECT name FROM department ');
-        const departmentStrArray = table[0].map(depObj => {
-            return depObj.name;
-        });
-
-        return departmentStrArray;
+    const departments = async () => {
+        const departmentList = await db.query('SELECT * FROM department');
+        return departmentList;
     }
-        const managers = async () => {
-            const table = await db.query('SELECT name FROM department ');
-            const departmentStrArray = table[0].map(depObj => {
-                return depObj.name;
-            });
+    const roles = async () => {
+        const roleList = await db.query('SELECT * FROM role');
+        return roleList;
+    }
+    const employees = async () => {
+        const employeeList = await db.query('SELECT * FROM employee');
+        return employeeList;
+    }
 
-            return departmentStrArray;
+    const managers = async () => {
+        const managerList = await db.query('SELECT first_name, last_name FROM employee WHERE manager_id = NULL');       
+        return managerList;
     }
     
         inquire.prompt(questions.main).then( async (answer) => {
             const choice = answer.main;
             switch (choice) {
                 case 'view all departments': 
-                    await db.query('SELECT first_name, last_name FROM employee WHERE id == manager_id', function (err, results) {
-                        const table = cTable.getTable(results);
+                        departList = await departments();
+                        table = cTable.getTable(departList[0]);
                         console.log(table);
                         start();
-                });
                     break;
                 case 'view all roles' :
-                    await db.query('SELECT * FROM role', function (err, results) {
-                        const table = cTable.getTable(results);
+                        let roleList = await roles();
+                        table = cTable.getTable(roleList[0]);
                         console.log(table);
                         start();
-                    });
                     break;
                 case 'view all employees' : 
-                    await db.query('SELECT * FROM employee', function (err, results) {
-                        const table = cTable.getTable(results);
+                        let employeeList = await employees();
+                        table = cTable.getTable(employeeList[0]);
                         console.log(table);
                         start();
-                    });
                     break;
                 case 'add a department' :
                     inquire.prompt(questions.addDepartment).then( (answer) => {
                         const department = answer.department;
-                        db.query('INSERT INTO department(name) VALUE (?)', department, (error, info) => {
-                            if (error) {
-                                console.log(error);
-                            } else {
-                                const table = cTable.getTable(info);
-                                console.log(department);
-                                console.log(table);
+                        db.query('INSERT INTO department(name) VALUE (?)', department).then(() => {
+                                console.log(`added ${department} to the database`);
                                 start();
-                            }
                         })
                     })
-                    
                     break;
                 case 'add a role' : 
-                    questions.addRole[2].choices = await departments();
+                    departList = await departments();
+                    const departmentStrArray = departList[0].map(depObj => {
+                        return depObj.name;
+                    });
+                    questions.addRole[2].choices = departmentStrArray;
+                    inquire.prompt(questions.addRole).then((answers) => {
+                        const roleName = answers.roleName;
+                        const roleSalary = answers.roleSalary;
+                        const roleDepartment = answers.roleDepartment;
+                        const departArray = questions.addRole[2].choices;
+                        let roleDepartmentId;
+                        departArray.forEach(department => {
+                            if (department == roleDepartment) {
+                                roleDepartmentId = departArray.indexOf(department, 0) + 1;
+                            }
+                        });
+                        db.query('INSERT INTO role (title, salary, department_id) VALUES ( ? , ? , ? )', [roleName, roleSalary, roleDepartmentId]).then(() => {
+                            console.log(`added ${roleName} to the database`);
+                            start();
+                        })
+                    })
+                    break;
+                case 'add an employee' :
+                    departList = await departments();
+                    roleList = await roles();
+                    let managerList = await managers();
+                    const departmentStrArray = departList[0].map(depObj => {
+                        return depObj.name;
+                    });
+                    questions.addRole[2].choices = departmentStrArray;
                     inquire.prompt(questions.addRole).then( (answers) => {
                         const roleName = answers.roleName;
                         const roleSalary = answers.roleSalary;
@@ -88,20 +109,11 @@ async function start() {
                                 roleDepartmentId = departArray.indexOf(department, 0) + 1;
                             }
                         });
-                        db.query('INSERT INTO role (title, salary, department_id) VALUES ( ? , ? , ? )', [roleName, roleSalary, roleDepartmentId], (error, info) => {
-                            if (error) {
-                                console.log(error);
-                            } else {
-                                const table = cTable.getTable(info);
-                                console.log(department);
-                                console.log(table);    /// since db.query is now a promise, form mysql2 i believe we no longer need the callback function so we can move all this ot the outside of the db.query, otherwise it will go back to Start();
-                                start();
-                            }
+                        db.query('INSERT INTO role (title, salary, department_id) VALUES ( ? , ? , ? )', [roleName, roleSalary, roleDepartmentId]).then(() => {
+                            console.log(`added ${roleName} to the database`);
+                            start();
                         })
                     })
-                    break;
-                case 'add an employee' :
-
                     break;
                 case 'update an employee role' :
 
